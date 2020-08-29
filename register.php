5c93bed962c6147723a57437e1deef105dc94baf
MorganFreem0n <70452071+MorganFreem0n@users.noreply.github.com>
2020-08-29 15:53:56 -0500

    register.php
    The form used to reg a user

---
+++ register.php
@@ -0,0 +1,13 @@
+<h1>creaoo99soc</h1>
+<h3>Register: </h3>
+<title>Register</title>
+<form action="register_create.php" method="POST">
+    <input type="text" value="Username" name="reg-username">
+    <br>
+    <br>
+    <input type="text" value="Password" name="reg-password">
+    <br>
+    <br>
+    <input type="submit" value="Register">
+</form>
+<link href="style.css" rel="stylesheet" type="text/css" />
