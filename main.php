8726e7debb4e910fddac91f38945da7ffc3d4658
MorganFreem0n <70452071+MorganFreem0n@users.noreply.github.com>
2020-08-29 15:52:57 -0500

    main.php
    This file is main website design

---
+++ main.php
@@ -0,0 +1,11 @@
+<link href="style.css" rel="stylesheet" type="text/css" />
+<header>
+    <h3>
+        Welcome to creaoor99soc!
+    </h3>
+</header>
+<div class="home"><a href="post related/home.php">Home</a></div>
+<?php
+// This file is used for the host
+?>
+
