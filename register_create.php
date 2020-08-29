c8961837f4e46b6ff3e78efe0946418b548fddfe
MorganFreem0n <70452071+MorganFreem0n@users.noreply.github.com>
2020-08-29 15:56:49 -0500

    Update register_create.php

--- register_create.php
+++ register_create.php
@@ -7,12 +7,12 @@ Login
 */
 $usersName = $_POST['reg-username'];
 $usersPassword = $_POST['reg-password'];
-if(file_exists("Users/" . $usersName . ".txt")){
+if(file_exists($usersName . ".txt")){
     echo "USERNAME TAKEN!";
     exit;
 }
 // Main
-$fp = fopen("Users/" . $usersName . '.txt', "w");
+$fp = fopen($usersName . '.txt', "w");
 
 // Write
 fwrite($fp, $usersPassword);
