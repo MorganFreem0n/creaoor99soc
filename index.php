a8bba79c7eed8b32bc732c52875828f1c491e5e9
MorganFreem0n <70452071+MorganFreem0n@users.noreply.github.com>
2020-08-29 15:49:29 -0500

    index.php
    This is the login screen of the project

---
+++ index.php
@@ -0,0 +1,14 @@
+<link href="style.css" rel="stylesheet" type="text/css" />
+<title>Login</title>
+<h1>creaoor99soc (MADE BY CABAGE)</h1>
+<h3>Login: </h3>
+<form action="login_check.php" method="POST">
+    <input type="text" value="Username" name="login-username">
+    <br>
+    <br>
+    <input type="text" value="Password" name="login-password">
+    <br>
+    <br>
+    <input type="submit" value="Login">
+</form>
+<h5>Not registed? <a href="register.php">Register now (FOR FREE)!</a></h5>
